7bcf473cc806c30c073faacf39a983020dc1d1c5
David Inglebard RICQ <davidricq87@orange.fr>
2018-03-28 22:04:51 +0200

    Update dependencies version and update routing

--- config/router.js
+++ config/router.js
@@ -2,6 +2,9 @@ var router = require('express').Router();
 
 var MessageController = require("../controllers/MessageController.js");
 
-router.get('/messages', MessageController.getMessages);
-
-module.exports = router;
+module.exports.init = function(app,data_app,next)
+{
+  router.get('/messages', MessageController.getMessages);
+  app.use('/', router);
+	next();
+}
